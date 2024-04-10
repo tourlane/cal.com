fb3f62b44dbb914b747605fe556d306cef184f0a
Jakob Pupke <jakop@pm.me>
2024-04-10 22:48:36 +0530

    remove ts extension

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     console.log("Registering instrumentation for Node.js");
-    await import("./instrumentation.node.ts");
+    await import("./instrumentation.node");
   }
 }
