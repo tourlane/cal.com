c0ff3a4adc1a04e3f134cb0a79a90ad40028c29c
rublo <ruben.tugnolo@gmail.com>
2024-04-10 16:36:32 +0200

    otel integration works

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,20 +1,6 @@
-/* eslint-disable prettier/prettier */
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { registerOTel } from "@vercel/otel";
-
-// import { performance } from "universal_perf_hooks";
-
 export async function register() {
-  console.log("===========================================");
-  registerOTel({
-    serviceName: "your-service-name",
-    traceExporter: new OTLPTraceExporter({ url: "http://localhost:4318/v1/trace" }),
-  });
-  // if (process.env.NEXT_RUNTIME === "nodejs" || process.env.NEXT_RUNTIME === "edge") {
-  //   console.log("Registering instrumentation for Node.js");
-  //   await import("./instrumentation.node.ts");
-  // }
-  //  else if (process.env.NEXT_RUNTIME === "edge") {
-  //   await import("./instrumentation.browser.ts");
-  // }
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    console.log("Registering instrumentation for Node.js");
+    await import("./instrumentation.node.ts");
+  }
 }
