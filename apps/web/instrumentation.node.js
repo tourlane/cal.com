43380a21f72cff9110d8f860fe51cb161d2e71e1
Jakob Pupke <jakop@pm.me>
2024-04-11 12:34:04 +0530

    prisma instrumentation

--- apps/web/instrumentation.node.js
+++ apps/web/instrumentation.node.js
@@ -2,11 +2,12 @@ import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { PrismaInstrumentation } from "@prisma/instrumentation";
 
 const sdk = new NodeSDK({
   serviceName: "cal.com",
   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [getNodeAutoInstrumentations(), new PrismaInstrumentation()],
 });
 
 sdk.start();
